98cac831b7b6f6d770c6d784d373945afbddfc60
lexucuflexu <alex.tenche@gmail.com>
2014-11-12 09:48:47 +0200

    added file upload

--- Shoutit Shoutbox/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// connect to MySQL
-$con = mysqli_connect("localhost", "root", "termopane", "shoutit");
-
-// test connection
-if(mysqli_connect_errno()){
-	echo "failed to connect to MySQL: ". mysqli_connect_error();
-}
--- file_write.php
+++ file_write.php
@@ -1,10 +1,10 @@
 <?php
 
 $file = 'filetest.txt';
-if($handle = fopen($file, 'w')) { // overwrite
+if($handle = fopen($file, 'a')) { // overwrite
 
-	fwrite($handle, 'abc'); // returns number of bytes or false
-	$content = "123\n456";  // double quotes matter (with \n)
+	//fwrite($handle, 'abc'); // returns number of bytes or false
+	$content = "\npare ca merge";  // double quotes matter (with \n)
 	fwrite($handle, $content);
 	
 	fclose($handle);
@@ -12,10 +12,12 @@ if($handle = fopen($file, 'w')) { // overwrite
 	echo "Could not open file for writing.";
 }
 
+
+/*
 // file_put_contents: shortcut for fopen/fwrite/fclose
 // overwrites existing file by default (so be CAREFUL)
 $file = 'filetest.txt';
 $content = "111\n222\nana are mere";
 if($size = file_put_contents($file, $content)) {
   echo "A file of {$size} bytes was created.";
-}
+}*/
