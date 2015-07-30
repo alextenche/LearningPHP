0f183f7347d5944e740373305bf445fda315b41f
Alex Tenche <alex.tenche@gmail.com>
2015-07-30 18:34:10 +0300

    working with psr-1 php standard

---
+++ bestPractices/packages/src/Example.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alexandru
+ * Date: 29.07.2015
+ * Time: 20:09
+ */
+
+namespace AlexTenche\Example;
+
+class Example
+{
+    public function getSomething()
+    {
+        return 'something intrestingh was loaded here';
+    }
+}
