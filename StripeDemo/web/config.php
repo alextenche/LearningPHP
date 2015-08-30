35f2a30e409aa5a933c13c300f43e8e3c03acce2
Alex Tenche <alex.tenche@gmail.com>
2015-08-30 11:19:57 +0300

    app working

--- StripeDemo/web/config.php
+++ StripeDemo/web/config.php
@@ -1,7 +1,11 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Alexandru
- * Date: 19.08.2015
- * Time: 21:23
- */
+
+require_once('../vendor/autoload.php');
+//require_once('../vendor/stripe/stripe-php/lib/Stripe.php');
+
+$stripe = array(
+  "secret_key"      => "sk_test_xGLTy8wSDjAT0urwK850iL1h",
+  "publishable_key" => "pk_test_XO9PVj9qT1kXB3AsCZUlo0uG"
+);
+
+\Stripe\Stripe::setApiKey($stripe['secret_key']);
