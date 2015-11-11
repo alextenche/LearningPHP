ad06e4f11225d75019b86e4741d346ed597707f6
Alex Tenche <alex.tenche@gmail.com>
2015-11-11 21:56:54 +0200

    added files for regex tutorial

--- sweets/addmember.php
+++ sweets/addmember.php
@@ -1,4 +1,34 @@
-<?php // some code ?>
+<?php
+// assign defaults
+$data =  array('email'      => 'email',
+							 'firstname'  => 'firstname',
+							 'lastname'   => 'lastname',
+							 'address'    => 'address',
+							 'city'       => 'city',
+							 'postcode'   => 'postcode',
+							 'telephone'  => 'telephone'
+);
+
+$errors =  array('email'      => '',
+						 		 'firstname'  => '',
+						 		 'lastname'   => '',
+						 		 'address'    => '',
+								 'city'       => '',
+			    			 'postcode'   => '',
+						 		 'telephone'  => ''
+);
+
+if(isset($_POST['data'])) {
+	$data = $_POST['data'];
+	foreach ($data as $key => $value) {
+		$data[$key] = strip_tags($value);
+	}
+	if(!preg_match('/^[a-z][a-z0-9._-]+@(\w+\.)+[a-z]{2,6}$/i', $data['email'])){
+		$error['email'] = '<b class="error"> Invalid email address</b>';
+	}
+}
+
+?>
 <!DOCTYPE HTML>
 <html>
 <head>
@@ -8,68 +38,72 @@
 <meta name="keywords" content="">
 <link rel="stylesheet" href="css/main.css" type="text/css">
 <link rel="shortcut icon" href="images/favicon.ico?v=2" type="image/x-icon" />
+<style>
+	.error { color: red;}
+</style>
 </head>
 <body>
 <div id="wrapper">
 	<div id="maincontent">
-		
+
 	<div id="header">
 		<div id="logo" class="left">
 			<a href="index.php"><img src="images/logo.png" alt="SweetsComplete.Com"/></a>
 		</div>
 		<div class="right marT10">
 			<b>
-			<a href="login.html" >Login</a> |<a href="members.html" >Our Members</a> |<a href="cart.html" >Shopping Cart</a>
+			<a href="login.html" >Login</a> |<a href="members.php" >Our Members</a> |<a href="cart.php" >Shopping Cart</a>
 			</b>
 			<br />
 			Welcome Guest		</div>
 		<ul class="topmenu">
-		<li><a href="home.html">Home</a></li>
-		<li><a href="products.html">Products</a></li>
-		<li><a href="specials.html">Specials</a></li>
-		<li><a href="contact.html">Contact Us</a></li>
+		<li><a href="index.php">Home</a></li>
+		<li><a href="products.php">Products</a></li>
+		<li><a href="specials.php">Specials</a></li>
+		<li><a href="contact.php">Contact Us</a></li>
 		</ul>
 		<br>
 		<div class="banner"><p></p></div>
 		<br class="clear"/>
 	</div> <!-- header -->
-		
+
 	<div class="content">
 	<br/>
 	<div class="product-list">
-		
+
 		<h2>Sign Up</h2>
 		<br/>
-		
+
 		<b>Please enter your information.</b><br/><br/>
-		<form>
+		<form action="addmember.php" method="post">
 			<p>
 				<label>Email: </label>
-				<input type="text" name="data[email]" />
+				<input type="text" name="data[email]" value="<?php echo htmlspecialchars($data['email']); ?>" />
+				<?php if($error['email']) echo '<p>', $error['email']; ?>
 			<p>
 			<p>
 				<label>First Name: </label>
-				<input type="text" name="data[firstname]" />
+				<input type="text" name="data[firstname]" value="<?php echo htmlspecialchars($data['firstname']); ?>"/>
 			<p>
 			<p>
 				<label>Last Name: </label>
-				<input type="text" name="data[lastname]" />
+				<input type="text" name="data[lastname]" value="<?php echo htmlspecialchars($data['lastname']); ?>" />
 			<p>
 			<p>
 				<label>Address: </label>
-				<input type="text" name="data[address]" />
+				<input type="text" name="data[address]" value="<?php echo htmlspecialchars($data['address']); ?>"/>
 			<p>
 			<p>
 				<label>City: </label>
-				<input type="text" name="data[city]" />
+				<input type="text" name="data[city]" value="<?php echo htmlspecialchars($data['city']); ?>"/>
 			<p>
 			<p>
 				<label>Postcode: </label>
-				<input type="text" name="data[postcode]" />
+				<input type="text" name="data[postcode]" value="<?php echo htmlspecialchars($data['postcode']); ?>"/>
 			<p>
 			<p>
 				<label>Telephone: </label>
-				<input type="text" name="data[telephone]" />
+				<input type="text" name="data[telephone]" value="<?php echo htmlspecialchars($data['telephone']); ?>"/>
 			<p>
 			<p>
 				<input type="reset" name="data[clear]" value="Clear" class="button"/>
@@ -78,7 +112,7 @@
 		</form>
 	</div><!-- product-list -->
 </div>
-	
+
 	</div><!-- maincontent -->
 
 	<div id="footer">
@@ -90,7 +124,7 @@
 			Email:sales@sweetscomplete.com</span>
 		div>
 	</div><!-- footer -->
-	
+
 </div><!-- wrapper -->
 
 </body>
