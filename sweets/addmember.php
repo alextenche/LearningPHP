d6a3280fbd1aeb4f2c251c10ddb092c9f4564ef6
Alex Tenche <alex.tenche@gmail.com>
2015-11-22 13:52:54 +0200

    added regex validation to sweets addmember.php

--- sweets/addmember.php
+++ sweets/addmember.php
@@ -9,7 +9,7 @@ $data =  array('email'      => 'email',
 							 'telephone'  => 'telephone'
 );
 
-$errors =  array('email'      => '',
+$error =  array('email'      => '',
 						 		 'firstname'  => '',
 						 		 'lastname'   => '',
 						 		 'address'    => '',
@@ -25,6 +25,24 @@ if(isset($_POST['data'])) {
 	}
 	if(!preg_match('/^[a-z][a-z0-9._-]+@(\w+\.)+[a-z]{2,6}$/i', $data['email'])){
 		$error['email'] = '<b class="error"> Invalid email address</b>';
+	}
+	if(!preg_match('/^[a-z0-9,. ]+$/i', $data['firstname'])){
+		$error['firstname'] = '<b class="error"> Name should only contain letters, numbers, spaces ", or ","</b>';
+	}
+	if(!preg_match('/[a-z0-9,. ]/i', $data['lastname'])){
+		$error['lastname'] = '<b class="error"> Name should only contain letters, numbers, spaces ", or ","</b>';
+	}
+	if(!preg_match('/[a-z0-9,. ]/i', $data['address'])){
+		$error['address'] = '<b class="error"> Address should only contain letters, numbers, spaces ", or ","</b>';
+	}
+	if(!preg_match('/[a-z0-9,. ]/i', $data['city'])){
+		$error['city'] = '<b class="error"> City should only contain letters, numbers, spaces ", or ","</b>';
+	}
+	if(!preg_match('/^[a-z][0-9][a-z] [0-9][a-z][0-9]$|^\d{5} (-\d{4})?$/i', $data['postcode'])){
+		$error['postcode'] = '<b class="error"> Canadian Postcode: A9A 9A9 <br>US Postcode: 99999 or 99999-9999</b>';
+	}
+	if(!preg_match('/^\+[0-9]{1,3} \d{3}-\d{3}-\d{4}/i', $data['telephone'])){
+		$error['telephone'] = '<b class="error"> Telephone numbers shoul be in form +CC AAA-CCC-DDDD</b>';
 	}
 }
 
@@ -84,26 +102,32 @@ if(isset($_POST['data'])) {
 			<p>
 				<label>First Name: </label>
 				<input type="text" name="data[firstname]" value="<?php echo htmlspecialchars($data['firstname']); ?>"/>
+				<?php if($error['firstname']) echo '<p>', $error['firstname']; ?>
 			<p>
 			<p>
 				<label>Last Name: </label>
 				<input type="text" name="data[lastname]" value="<?php echo htmlspecialchars($data['lastname']); ?>" />
+				<?php if($error['lastname']) echo '<p>', $error['lastname']; ?>
 			<p>
 			<p>
 				<label>Address: </label>
 				<input type="text" name="data[address]" value="<?php echo htmlspecialchars($data['address']); ?>"/>
+				<?php if($error['address']) echo '<p>', $error['address']; ?>
 			<p>
 			<p>
 				<label>City: </label>
 				<input type="text" name="data[city]" value="<?php echo htmlspecialchars($data['city']); ?>"/>
+				<?php if($error['city']) echo '<p>', $error['city']; ?>
 			<p>
 			<p>
 				<label>Postcode: </label>
 				<input type="text" name="data[postcode]" value="<?php echo htmlspecialchars($data['postcode']); ?>"/>
+				<?php if($error['postcode']) echo '<p>', $error['postcode']; ?>
 			<p>
 			<p>
 				<label>Telephone: </label>
 				<input type="text" name="data[telephone]" value="<?php echo htmlspecialchars($data['telephone']); ?>"/>
+				<?php if($error['telephone']) echo '<p>', $error['telephone']; ?>
 			<p>
 			<p>
 				<input type="reset" name="data[clear]" value="Clear" class="button"/>
@@ -117,12 +141,12 @@ if(isset($_POST['data'])) {
 
 	<div id="footer">
 		<div class="footer">
-			Copyright &copy; 2012 sweetscomplete.com. All rights reserved. <br/>
+			Copyright &copy; 2015 sweetscomplete.com. All rights reserved. <br/>
 		<a href="home">Home</a> | <a href="products">Products</a> | <a href="specials">Specials</a> | <a href="contact">Contact Us</a> 		<br />
 			<span class="contact">Tel: +44-1234567890&nbsp;
 			Fax: +44-1234567891&nbsp;
 			Email:sales@sweetscomplete.com</span>
-		div>
+		</div>
 	</div><!-- footer -->
 
 </div><!-- wrapper -->
