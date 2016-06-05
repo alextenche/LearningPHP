f84cf0fb501d04b7022e3a352ac6c57d2f591a32
Alex Tenche <alex.tenche@gmail.com>
2016-06-05 13:46:54 +0300

    work on cookbook

---
+++ cookbookOOP/classes/recipe.php
@@ -0,0 +1,121 @@
+<?php
+
+class Recipe
+{
+  private $title;
+  private $ingredients = array();
+  private $instructions = array();
+  private $yield;
+  private $tags = array();
+  private $source = "Alina Holligan";
+
+  private $measurments = array(
+    "tsp",
+    "tbsp",
+    "cup",
+    "oz",
+    "lb",
+    "fl oz",
+    "pint",
+    "quart",
+    "gallon"
+  );
+
+  public function __construct($title = null){
+    if(empty($title)){
+      $this->title = null;
+    } else {
+      $this->title = ucwords($title);
+    }
+  }
+
+  public function __toString(){
+    $output = "You are calling a " . __CLASS__ . " object with the title \"" . $this->getTitle() ."\"";
+    $output .= "\nIt is stored in " . basename(__FILE__) . " at " . __DIR__ . ".";
+    $output .= "\nThis display is from line " . __LINE__ . " in method " . __METHOD__ . ".";
+    $output .= "\nThe following methods are available for objects in this class:";
+    $output .= implode("\n", get_class_methods(__CLASS__));
+    return $output;
+  }
+
+  public function setTitle($title)
+  {
+    $this->title = ucwords($title);
+  }
+
+  public function getTitle()
+  {
+    return $this->title;
+  }
+
+  public function addIngredient($item, $amount = null, $measure = null){
+    if($amount != null && !is_float($amount) && !is_int($amount)){
+      exit("Amount must be a float: " . gettype($amount) . " $amount given");
+    }
+    if($measure != null && !in_array(strtolower($measure), $this->measurments)){
+      exit("Please enter a valid measurment: " . implode(", ", $this->measurments));
+    }
+    $this->ingredients[] = array(
+      "item" => ucwords($item),
+      "amount" => $amount,
+      "measure" => strtolower($measure)
+    );
+  }
+
+  public function getIngredients(){
+    return $this->ingredients;
+  }
+
+  public function addInstruction($string){
+    $this->instructions[] = $string;
+  }
+
+  public function getInstructions(){
+    return $this->instructions;
+  }
+
+  public function addTag($tag){
+    $this->tags[] = strtolower($tag);
+  }
+
+  public function getTags(){
+    return $this->tags;
+  }
+
+  public function setYield($yield){
+    $this->yield = $yield;
+  }
+
+  public function getYield(){
+    return $this->yield;
+  }
+
+  public function setSource($source){
+    $this->source = ucwords($source);
+  }
+
+  public function getSource(){
+    return $this->source;
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ cookbookOOP/cookbook.php
@@ -0,0 +1,6 @@
+<?php
+require_once("classes/recipe.php");
+require_once("classes/render.php");
+require_once("inc/recipes.php");
+
+echo Render::displayRecipe($belgian_waffles);
